7d5f616e9e50e745b1f7ae17926604612dcd50b4
devan <devanharikumar89@gmail.com>
2016-08-22 21:49:27 -0400

    bug fix

--- code/1/dhariku.py
+++ code/1/dhariku.py
@@ -2,9 +2,9 @@ def testPrime(input):
 	number=int(input)
 	if number<=2:
 		return False;
-	for i in range(2, int(input**(0.5))):
+	for i in range(2, int(input**(0.5))+1):
 		if number%i==0:
 			return False;
 	return True;
 
-print testPrime(172);	
+print testPrime(9);	
