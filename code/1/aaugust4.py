be19ab578f5ac263c98ff9bcac534dec2b04a1a9
aaugust4 <arjunaugustine@gmail.com>
2016-08-22 21:30:08 -0400

    Refined test_two

--- code/1/aaugust4.py
+++ code/1/aaugust4.py
@@ -3,5 +3,6 @@
 def testTrue(boolString):
   trueStrings = {"true", "True", "TRUE", "T", "t"}
   result = any(boolString in trueString for trueString in trueStrings)
-  print (result)
+  result = boolString in trueStrings 
+  print ("Input string: " + boolString + "\t Result: " + str(result))
   return result
