f2452ac679a4113de94ed832949804382872ab0d
aaugust4 <arjunaugustine@gmail.com>
2016-09-30 23:00:21 -0400

    Add more code

--- code/5/problem.py
+++ code/5/problem.py
@@ -70,6 +70,7 @@ class Point(O):
     O.__init__(self)
     self.decisions = decisions
     self.objectives = None
+    self.energy = None
 
   def __eq__(self, other):
     return self.decisions == other.decisions
@@ -89,12 +90,14 @@ class Problem(O):
     self.decisions.append(Decision('x5', 1, 05))
     self.decisions.append(Decision('x6', 0, 10))
     self.objectives = [Objective('f1', True), Objective('f2', True)]
+    self.points = []
 
   def evaluate(self, point):
     [x1, x2, x3, x4, x5, x6] = self.decisions
     f1 = -(25*((x1-2)**2) + (x2-2)**2 + (x3-1)**2 * (x4-4)**2 + (x5-1)**2)
     f2 = x1**2 + x2**2 + x3**2 + x4**2 + x5**2 + x6**2
     point.objectives = [f1, f2]
+    point.energy = f1 + f2
     return point.objectives
 
   @staticmethod
@@ -104,13 +107,57 @@ class Problem(O):
     :param point:
     :return: True
     """
+    [x1, x2, x3, x4, x5, x6] = point.decisions
+    if x1 + x2 -2 < 0: return False
+    if 6 - x1 - x2 < 0: return False
+    if 2 - x2 + x1 < 0: return False
+    if 2 - x1 + 3*x2 < 0: return False
+    if 4 - (x3 - 3)**2 - x4 < 0: return False
+    if (x5 - 3)**3 + x6 - 4 < 0: return False
     return True
 
   def generate_one(self):
+    """
+    Generate a valid random point, evaluate it and add it to points list.
+    :return: generated point
+    """
     while True:
       mypoint = Point([random.randint(d.low, d.high) for d in self.decisions])
       if self.is_valid(mypoint):
+        self.evaluate(mypoint)
+        self.points.append(mypoint)
         return mypoint
 
+
+
+
+class MaxWalkSat(O):
+  """
+  FOR i = 1 to max-tries DO
+  solution = random assignment
+  FOR j =1 to max-changes DO
+    IF  score(solution) > threshold
+        THEN  RETURN solution
+    FI
+    c = random part of solution
+    IF    p < random()
+    THEN  change a random setting in c
+    ELSE  change setting in c that maximizes score(solution)
+    FI
+  RETURN failure, best solution found
+  """
+  def __init__(self, problem):
+    O.__init__(self)
+    self.Prob = problem()
+    self.P = 0.5
+    self.MAX_TRIES = 100
+    self.MAX_CHANGES = 100
+    self.best = self.Prob.generate_one()
+
+  def run(self, problem):
+    for i in xrange(self.MAX_TRIES):
+      solution = problem.generate_one() # Generate and evaluate a new point.
+      for j in xrange(self.MAX_CHANGES):
+        c = random.choice(solution.decisions)
 
 
